613d540d1c40730600b0e694ee37cc605d8c6195
Viktoriia Zubkova <vichgirl@gmail.com>
2026-01-06 09:23:43 +0300

    feat: option and arguments was added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,6 @@ program
   .name('Difference Evaluator')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]',  'output format')
   .parse(process.argv)
